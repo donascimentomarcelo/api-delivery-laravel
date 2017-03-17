9fb1c14f19bf69ffc42e25f3f6d056ee1d6d317c
marcelojunin <marcelojunin2010@hotmail.com>
2017-03-16 22:31:54 -0300

    ClientService funcionando

--- app/Services/ClientService.php
+++ app/Services/ClientService.php
@@ -20,6 +20,10 @@ class ClientService
 	public function update(array $data, $id)
 	{
 		$this->clientRepository->update($data, $id);
+
+		$userId = $this->clientRepository->find($id, ['user_id'])->user_id;
+		//recupera o fk em client id pq o primeiro $id q ta sendo passado como parametro é o id do client
+		$this->userRepository->update($data['user'], $userId);
 	}
 }
 
