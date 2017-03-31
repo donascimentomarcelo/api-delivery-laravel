e74771222253cf6d2e8f8a4173a3fdd7355ff654
Marcelo <marcelojunin2010@hotmail.com>
2017-03-31 15:58:48 -0300

    Angular-OAuth2, Ui-router, Login

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic','angular-oauth2'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,28 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider, OAuthProvider, OAuthTokenProvider){
+   
+   OAuthTokenProvider.configure({
+    name: 'token',
+    options: {
+      secure: false
+    }
+  });
+
+   OAuthProvider.configure({
+      baseUrl: 'https://localhost:8000',
+      clientId: 'appid01',
+      clientSecret: 'secret' // optional
+  });
+
+  $stateProvider
+  .state('login',{
+    url: '/login',
+    templateUrl:'templates/login.html',
+    controller:'LoginCtrl'
+  })
+
+  // $urlRouterProvider.otherwise('/');
+});
---
+++ ionic/www/js/controllers/loginCtrl.js
@@ -0,0 +1,7 @@
+app.controller('LoginCtrl', function($scope,$stateParams){
+	
+      $scope.login = function(){
+
+      };
+    
+});
