4b04f418d648995c4fb74b9ceee741ab4bf629de
Marcelo <marcelojunin2010@hotmail.com>
2017-04-24 10:37:01 -0300

    Erro do non-object no event

--- app/Events/GetLocationDeliveryman.php
+++ app/Events/GetLocationDeliveryman.php
@@ -25,7 +25,6 @@ class GetLocationDeliveryman extends Event implements ShouldBroadcast
      */
     public function __construct(Geo $geo, Order $order)
     {
-        //
         $this->geo = $geo;
         $this->order = $order;
     }
